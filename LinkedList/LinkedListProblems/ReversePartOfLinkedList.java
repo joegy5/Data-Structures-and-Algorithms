8d7b6f352681bc664c2ca9979465eb9054c3be87
joegy5 <69650123+joegy5@users.noreply.github.com>
2022-06-17 17:34:54 -0500

    Update ReversePartOfLinkedList.java

--- LinkedList/LinkedListProblems/ReversePartOfLinkedList.java
+++ LinkedList/LinkedListProblems/ReversePartOfLinkedList.java
@@ -8,6 +8,9 @@
 // EX: Input: head = [5], left = 1, right = 1
 //     Output: [5]
 
+// TIME COMPLEXITY: O(n)
+// SPACE COMPLEXITY: O(1)
+
 
 /**
  * Definition for singly-linked list.
@@ -29,6 +32,8 @@ class Solution {
         
         ListNode temp = head;
         
+        // Create a dummy node for the cases where the left position is just 1, to avoid having to make more code just to handle that specific case 
+        
         if(left == 1) {
             ListNode dummy = new ListNode(0, head);
             head = dummy;
@@ -44,11 +49,16 @@ class Solution {
             }       
         }
         
+        // Keep track of the node immediately to the left of the position so that it can be linked to the new first node of the newly reversed segment
+        // of the linked list
+        
         ListNode leftPrevious = temp;
         temp = temp.next;
         ListNode next = temp.next;
         ListNode leftNode = temp;
         
+        
+        // Reverse the segment of the list 
         while(i < right-1) {
             ListNode nextNext = next.next;
             
@@ -61,6 +71,8 @@ class Solution {
         leftNode.next = next;
         leftPrevious.next = temp;
         
+        // Make sure to return the original head of the linked list if it has a dummy node due to the left position being equal to 1. Otherwise, return "head" 
+        // (as it has not changed from its original value)
         if(left == 1) {
             return head.next;
         }
