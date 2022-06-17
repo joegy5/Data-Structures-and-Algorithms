97805a5886f3325d48a10fa38f0f0dfc744581bf
joegy5 <69650123+joegy5@users.noreply.github.com>
2022-06-17 17:11:55 -0500

    Update Node.java

--- LinkedList/LinkedListImplementation/Node.java
+++ LinkedList/LinkedListImplementation/Node.java
@@ -1,1 +1,35 @@
+package com.company;
+
+public class Node {
+
+    private String name;
+    private Node next;
+
+    public Node(String name, Node next) {
+        this.name = name;
+        this.next = next;
+    }
+
+    public Node(String name) {
+        this.name = name;
+        this.next = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
 
