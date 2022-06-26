f5f8074e2394917fb59b55693d6830d2e118c321
joegy5 <69650123+joegy5@users.noreply.github.com>
2022-06-25 22:50:13 -0500

    Update LongestPalindromicSubsequence.java

--- Dynamic Programming/LongestPalindromicSubsequence.java
+++ Dynamic Programming/LongestPalindromicSubsequence.java
@@ -1,3 +1,38 @@
+// PROBLEM DESCRIPTION:
+// Given a string s, find the longest palindromic subsequence's length in s.
+
+// A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+// Example 1:
+
+// Input: s = "bbbab"
+// Output: 4
+// Explanation: One possible longest palindromic subsequence is "bbbb".
+
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: 2
+// Explanation: One possible longest palindromic subsequence is "bb".
+
+// NOTE: I did not come up with this solution on my own. 
+
+// INTUITION:
+// This is similar to the longest common subsequence problem 
+// Like the longest common subsequence problem, we want to find the longest subsequence that meets the conditions.
+// In this case, we can have two pointers, one that starts at the beginning of the string and one that starts at the end of the string. 
+// If the characters that the two pointers are looking at are the same, that means we can add 2 to the current palindromic subsequence that we have. 
+// Otherwise, we want to look at both cases where in one case we increment the left pointer and keep the right pointer the same,
+// and the other case where we decrement the right pointer and keep the left pointer the same. 
+// From there, we want to take the maximum length of the two subsequences from the segments of the string formed by those two cases.
+// This is our recurrence relation that we can use to recurse from one subproblem to the next.
+// Once again, we use a storage to memoize our results to avoid unnecessary recomputations. 
+
+// TIME COMPLEXITY: O(N squared), where N is the length of the string
+// SPACE COMPLEXITY: O(N squared), where N is the length of the string. 
+
 class Solution {
     int[][] memo;
     String s;
